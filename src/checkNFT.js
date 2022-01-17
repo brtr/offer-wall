474bf6b2c50a289a912db5993287d46fd8757bad
iannono <xiongbo027@gmail.com>
2022-01-17 15:03:43 +0800

    update script to accept arguments

--- src/checkNFT.js
+++ src/checkNFT.js
@@ -6,11 +6,14 @@ const serverUrl = SERVER_URL;
 const appId = APP_ID;
 Moralis.start({ serverUrl, appId });
 
-const CHAIN = ""
-const userAddress = ""
-const tokenAddress = ""
-
+// node src/checkNFT.js "polygon" "0x5CaCCe1B2c65606175469bCD3E6F4fbe4D0fb63c" "0x938a6942bd09cfac1bc4b2420f581a90fb5d5775"
 async function main() {
+  const args = process.argv.slice(2);
+
+  const CHAIN = args[0]
+  const userAddress = args[1]
+  const tokenAddress = args[2]
+
   const options = { chain: CHAIN, address: userAddress, token_address: tokenAddress};
   const nfts = await Moralis.Web3API.account.getNFTsForContract(options);
   console.log(nfts.result.length > 0);
